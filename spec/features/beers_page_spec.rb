594166e398a3d01472fcfd965885d8fc6760b797
Teemu Oksanen <git@teemuoksanen.fi>
2022-12-22 23:13:45 +0200

    Viikko 4, tehtävä 5 tehty

---
+++ spec/features/beers_page_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "Beer" do
+  let!(:brewery) { FactoryBot.create :brewery, name: "Koff" }
+
+  it "is created with valid information" do
+    visit new_beer_path
+
+    fill_in('beer[name]', with: 'Testiolut')
+    select('Lager', from: 'beer[style]')
+    select('Koff', from: 'beer[brewery_id]')
+    click_button('Create Beer')
+
+    expect(page).to have_content 'Beers'
+    expect(page).to have_content 'Testiolut'
+  end
+
+  it "is not created if no name is given" do
+    visit new_beer_path
+
+    select('Lager', from: 'beer[style]')
+    select('Koff', from: 'beer[brewery_id]')
+    click_button('Create Beer')
+
+    expect(Beer.count).to eq(0)
+    expect(page).to have_content "New beer"
+    expect(page).to have_content "Name can't be blank"
+  end
+end
