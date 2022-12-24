66799c5ffd9ff63cc7dd9510d4901b28d204745d
Teemu Oksanen <git@teemuoksanen.fi>
2022-12-25 01:02:41 +0200

    Viikko 5, ennen tehtävää 5

---
+++ spec/features/places_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe "Places" do
+  it "if one is returned by the API, it is shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [ Place.new( name: "Oljenkorsi", id: 1 ) ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+  end
+
+  it "if many are returned by the API, all are shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [ Place.new( name: "Oljenkorsi", id: 1 ), Place.new( name: "Parmesan", id: 2 ) ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+    expect(page).to have_content "Parmesan"
+  end
+
+  it "if none is returned by the API, appropriate note is shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [  ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "No locations in kumpula"
+  end
+end
